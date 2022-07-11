17b0afc5826a9226956abd0cf728a7002d900e83
Ramirez <ramirez.alberto@zoho.com>
2022-07-10 21:59:15 -0700

    Add BinaryTree project

--- Data_Estructuras/Program.cs
+++ Data_Estructuras/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BinaryTree;
+
+BinarySearchTree tree = new BinarySearchTree();
+int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+tree.root = tree.GenerateBST(array, 0, array.Length - 1);
+tree.preOrder(tree.root);
