a5183c2346cf3df2e8aecd65e3104d13aca8283c
Ramirez <ramirez.alberto@zoho.com>
2022-07-12 13:43:52 -0700

    Add a test for preorder navigation method in a BST

--- DataStructures.Tests/ValidityTests.cs
+++ DataStructures.Tests/ValidityTests.cs
@@ -5,21 +5,20 @@ namespace DataStructuresTests
 {
     public class ValidityTests
     {
-        [Fact]
-        public void GenerateBSTTest()
+        [Theory]
+        [InlineData("4 2 1 3 6 5 7 ", new int[] { 1, 2, 3, 4, 5, 6, 7 })]
+        [InlineData("2 1 3 ", new int[] { 1, 2, 3 })]
+        public void Test_preOrderMethod(string expected, int[] arr)
         {
             //Arrange
-
             BinarySearchTree tree = new BinarySearchTree();
-            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7};
+            tree.root = tree.GenerateBST(arr, 0, arr.Length - 1);
 
             //Act
-
-            tree.root = tree.GenerateBST(array, 0, array.Length - 1);
             string preOrder = tree.preOrder(tree.root);
 
             //Assert
-            Assert.Equal("4 2 1 3 6 5 7 ", preOrder);
+            Assert.Equal(expected, preOrder);
 
         }
     }
